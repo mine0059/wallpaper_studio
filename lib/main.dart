07f86bdea9005a19ec8f90175a17dc84041e70e8
ommanuel <eonojefemue@bluetag.tech>
2025-11-12 23:20:41 +0100

    update: main file to use system ui overlay

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wallpaper_studio/core/common/constants/colors.dart';
-import 'package:wallpaper_studio/screens/home_screen.dart';
+import 'package:wallpaper_studio/screens/splash_screen.dart';
+// import 'package:wallpaper_studio/screens/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    // Status bar (top)
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.light, // Dark icons
+    statusBarBrightness: Brightness.dark, // iOS
+
+    // Navigation bar (bottom)
+    systemNavigationBarColor: Colors.white, // Match your bottom nav
+    systemNavigationBarIconBrightness: Brightness.dark, // Dark icons
+    systemNavigationBarDividerColor: Colors.transparent,
+  ));
+
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+  );
   runApp(const MyApp());
 }
 
@@ -18,7 +36,8 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Poppins',
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      // home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
