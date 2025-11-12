836cb643b049b1c8b1f7c3c04ba3c1ec35344b1e
ommanuel <eonojefemue@bluetag.tech>
2025-11-12 23:19:08 +0100

    update: color for crypto views

---
+++ lib/core/common/constants/crypto_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CColors {
+  // App theme colors
+  static const Color primary = Color(0xff000000);
+  static const Color secondary = Color.fromARGB(103, 29, 29, 215);
+
+  // Text colors
+  static const Color primaryTextColor = Color(0xff000000);
+  static const Color secondaryTextColor = Color(0xff575757);
+
+  // Background colors
+  static const Color backgroundColor = Color.fromARGB(214, 248, 248, 248);
+
+  // Border colors
+  static const Color border = Color(0xffdfdfdf);
+
+  // Neutral Shades
+  static const Color white = Color(0xffffffff);
+  static const Color grey = Color(0xfff5f5f5);
+  static const Color darkgrey = Color(0xff808080);
+}
